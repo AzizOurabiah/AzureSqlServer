d5982840bcb018317913d466c34dc34740887d07
Aziz-Ourabiah-Hotmail <ourabiahaziz1@gmail.com>
2024-01-25 11:48:43 -0500

    Ajoutez des fichiers projet.

---
+++ Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace WepAppVisualStudio.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public int ProductName { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ Services/ProductService.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.SqlClient;
+
+namespace WepAppVisualStudio.Services
+{
+    public class ProductService
+    {
+        private static string db_source = "firstserveraziz.database.windows.net";
+        private static string db_user = "sqlserver";
+        private static string db_password = "Admin.123456789";
+        private static string db_database = "sqlserver";
+
+
+        private SqlConnection Connection()
+        {
+            var _builder = new SqlConnectionStringBuilder();
+
+            _builder.DataSource = db_source;
+            _builder.UserID = db_user;
+            _builder.Password = db_password;
+            _builder.InitialCatalog = db_database;
+
+            return new SqlConnection(_builder.ConnectionString);
+        }
+    }
+}
